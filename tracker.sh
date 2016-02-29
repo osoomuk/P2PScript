62f4926842b5dbe868f8d48bef6355843d171d90
osoomuk <ozim1343@gmail.com>
2016-02-29 22:25:07 +0200

    modified:   tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -1,4 +1,12 @@
-#/bin/bash 
+#!/bin/bash 
+
+echo $1
+echo $2
+if [ $UID -ne 0 ]; then
+	echo "Start this script $(basename $0) as a root"
+	exit 1
+fi
+#uurime faili nimed olemasolu
 if [ -z "$1" ]; then
 	echo "Please insert seed file name as a argument"
 	exit 0
@@ -6,12 +14,6 @@ fi
 if [ -z "$2" ]; then
 	echo "Please insert torrent file name as a argument"
 	exit 0
-fi
-echo $1
-echo $2
-if [ $UID -ne 0 ]; then
-	echo "Start this script $(basename $0) as a root"
-	exit 1
 fi
 #Kontrollime kas apache2 on
 APACHE=`/usr/bin/apt-cache policy apache2 | grep Installed | awk '{print $2}'`
@@ -39,12 +41,13 @@ if [ "$TOR" == "(none)" ]; then
 	apt-get install transmission-cli transmission-daemon -y
 	echo "Transmission installed"
 fi
-transmission-create -o $2 -t http://192.168.0.101:80/ui.php 
-$1
+transmission-create -o $2 -t http://192.168.0.101:80/ui.php $1
 echo "torrent file made"
 RTOR=`/usr/bin/apt-cache policy rtorrent | grep Installed | awk '{print $2}'`
+ls -a
 if [ "$RTOR" == "(none)" ]; then
 	apt-get install rtorrent -y
 	echo "Rtorrent installed"
-fi 
+fi
+chmod 777 $2  
 rtorrent $2
